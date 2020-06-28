6d8c9cbdbd6bce261b8438320810da350159da01
Thiago Gouvêa <thiagogouvea@gmail.com>
2020-06-28 12:15:24 -0300

    Remove MaterializeCSS and Yarn vulnerabilities

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
-
 import App from './App';
 
 ReactDOM.render(
